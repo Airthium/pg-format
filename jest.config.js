fc88a2bcc718fc2b42240fe861f78ee44538e5ec
Simon Garnotel <simon.garnotel@gmail.com>
2022-10-12 14:42:54 +0200

    v2.0.0

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const jestConfig = {
   rootDir: '.',
   testMatch: ['<rootDir>/**/*.test.ts'],
   transform: {
     '^.+\\.(t|j)s?$': '@swc/jest'
   }
 }
+
+export default jestConfig
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,6 @@
 import format from '../lib'
 
 describe('format', () => {
-  const testDate = new Date(Date.UTC(2012, 11, 14, 13, 6, 43, 152))
-  const testArray = ['abc', 1, true, null, testDate]
-  const testIdentArray = ['abc', 'AbC', 1, true, testDate]
-  const testObject = { a: 1, b: 2 }
   const testNestedArray = [
     [1, 2],
     [3, 4],
